d57e07edd69e6a37697bdeb5e293d6845d2f26e2
Yevgeniy Brikman <brikis98@gmail.com>
2017-11-27 14:39:28 +0000

    Add Keys helper

--- collections/maps.go
+++ collections/maps.go
@@ -1,5 +1,7 @@
 package collections
 
+import "sort"
+
 // Merge all the maps into one. Sadly, Go has no generics, so this is only defined for string to interface maps.
 func MergeMaps(maps ... map[string]interface{}) map[string]interface{} {
 	out := map[string]interface{}{}
@@ -13,3 +15,15 @@ func MergeMaps(maps ... map[string]interface{}) map[string]interface{} {
 	return out
 }
 
+// Return the keys for the given map, sorted alphabetically
+func Keys(m map[string]string) []string {
+	out := []string{}
+
+	for key, _ := range m {
+		out = append(out, key)
+	}
+
+	sort.Strings(out)
+
+	return out
+}
