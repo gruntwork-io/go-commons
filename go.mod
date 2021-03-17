35eb0477dd67dadb90e1eb67f4391d3842ea7ac7
Yoriyasu Yano <430092+yorinasub17@users.noreply.github.com>
2021-03-17 03:09:08 -0500

    Port retry package from kubergrunt

--- go.mod
+++ go.mod
@@ -6,11 +6,12 @@ require (
 	github.com/bgentry/speakeasy v0.1.0
 	github.com/fatih/color v1.9.0
 	github.com/go-errors/errors v1.0.2-0.20180813162953-d98b870cc4e0
+	github.com/gruntwork-io/kubergrunt v0.6.10
 	github.com/gruntwork-io/terratest v0.32.9
 	github.com/hashicorp/go-multierror v1.1.0
 	github.com/mattn/go-zglob v0.0.2-0.20190814121620-e3c945676326
-	github.com/sirupsen/logrus v1.4.2
-	github.com/stretchr/testify v1.4.0
-	github.com/urfave/cli v1.22.2
+	github.com/sirupsen/logrus v1.6.0
+	github.com/stretchr/testify v1.6.1
+	github.com/urfave/cli v1.22.4
 	golang.org/x/crypto v0.0.0-20210220033148-5ea612d1eb83
 )
---
+++ retry/retry.go
@@ -0,0 +1,80 @@
+package retry
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/sirupsen/logrus"
+)
+
+// DoWithRetry runs the specified action. If it returns a FatalError, return that error immediately.
+// If it returns any other type of error, sleep for sleepBetweenRetries and try again, up to a maximum of
+// maxRetries retries. If maxRetries is exceeded, return a MaxRetriesExceeded error.
+func DoWithRetry(
+	logger *logrus.Logger,
+	actionDescription string,
+	maxRetries int,
+	sleepBetweenRetries time.Duration,
+	action func() error,
+) error {
+	_, err := DoWithRetryInterface(
+		logger,
+		actionDescription,
+		maxRetries,
+		sleepBetweenRetries,
+		func() (interface{}, error) { return nil, action() },
+	)
+	return err
+}
+
+// DoWithRetryInterface runs the specified action. If it returns a value, return that value. If it returns a FatalError,
+// return that error immediately. If it returns any other type of error, sleep for sleepBetweenRetries and try again, up
+// to a maximum of maxRetries retries. If maxRetries is exceeded, return a MaxRetriesExceeded error.
+func DoWithRetryInterface(
+	logger *logrus.Logger,
+	actionDescription string,
+	maxRetries int,
+	sleepBetweenRetries time.Duration,
+	action func() (interface{}, error),
+) (interface{}, error) {
+	var output interface{}
+	var err error
+
+	for i := 0; i <= maxRetries; i++ {
+		logger.Infof(actionDescription)
+
+		output, err = action()
+		if err == nil {
+			return output, nil
+		}
+
+		if _, isFatalErr := err.(FatalError); isFatalErr {
+			logger.Infof("Returning due to fatal error: %v", err)
+			return output, err
+		}
+
+		logger.Infof("%s returned an error: %s. Attempt %d of %d. Sleeping for %s and will retry.", actionDescription, err.Error(), i+1, maxRetries, sleepBetweenRetries)
+		time.Sleep(sleepBetweenRetries)
+	}
+
+	return output, MaxRetriesExceeded{Description: actionDescription, MaxRetries: maxRetries}
+}
+
+// MaxRetriesExceeded is an error that occurs when the maximum amount of retries is exceeded.
+type MaxRetriesExceeded struct {
+	Description string
+	MaxRetries  int
+}
+
+func (err MaxRetriesExceeded) Error() string {
+	return fmt.Sprintf("'%s' unsuccessful after %d retries", err.Description, err.MaxRetries)
+}
+
+// FatalError is a marker interface for errors that should not be retried.
+type FatalError struct {
+	Underlying error
+}
+
+func (err FatalError) Error() string {
+	return fmt.Sprintf("FatalError{Underlying: %v}", err.Underlying)
+}
