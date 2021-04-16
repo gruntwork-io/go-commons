0e40a65c216519a604d772690c2ba0810da1e1bc
Yoriyasu Yano <430092+yorinasub17@users.noreply.github.com>
2021-04-16 12:24:44 -0500

    Rename aws to awscommons

--- aws/v2/doc.go
+++
@@ -1,4 +0,0 @@
-// Package aws contains routines for interacting with AWS. Meant to provide high level interfaces used throughout
-// various Gruntwork CLIs.
-// NOTE: The routines in this package are adapted for aws-sdk-go-v2, not V1.
-package aws
--- awscommons/v2/secretsmanager_test.go
+++ awscommons/v2/secretsmanager_test.go
@@ -1,4 +1,4 @@
-package aws
+package awscommons
 
 import (
 	"testing"
