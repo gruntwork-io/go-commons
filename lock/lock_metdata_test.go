b9e7a0742428944424e7a0a15ec0ff54762aaf30
Ina <ina.m.stoyanova@gmail.com>
2021-04-23 17:03:32 +0100

    Copied from terragrunt v0.6.2

---
+++ lock/lock_metdata_test.go
@@ -0,0 +1,34 @@
+package locks
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGetIpAddress(t *testing.T) {
+	t.Parallel()
+
+	ip, err := getIpAddress()
+
+	assert.Nil(t, err)
+	assertIsValidIp(t, ip)
+}
+
+func TestCreateLockMetadata(t *testing.T) {
+	t.Parallel()
+
+	expectedStateFileId := "expected-state-file-id"
+	expectedUsername := "jim"
+	lockMetadata, err := CreateLockMetadata(expectedStateFileId, expectedUsername)
+
+	assert.Nil(t, err)
+	assert.Equal(t, expectedStateFileId, lockMetadata.StateFileId)
+	assert.False(t, lockMetadata.DateCreated.IsZero())
+	assertIsValidIp(t, lockMetadata.IpAddress)
+	assert.Equal(t, expectedUsername, lockMetadata.Username)
+}
+
+func assertIsValidIp(t *testing.T, ip string) {
+	assert.Regexp(t, "\\d+.\\d+.\\d+.\\d+", ip)
+}
