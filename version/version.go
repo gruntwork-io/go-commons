3e433559816c571e81274fb0c36445c4409248bc
Ben Whaley <bwhaley@gmail.com>
2021-04-23 08:23:58 -0700

    Use conventional Go style for exported variables

--- version/version.go
+++ version/version.go
@@ -4,15 +4,15 @@ package version
 // to the latest Git tag when building our Go apps:
 //
 // build-go-binaries --app-name my-app --dest-path bin --ld-flags "-X github.com/gruntwork-io/go-commons/version.VERSION=$CIRCLE_TAG"
-// go build -o my-app -ldflags "-X github.com/gruntwork-io/go-commons/version.VERSION=v1.0.0"
+// go build -o my-app -ldflags "-X github.com/gruntwork-io/go-commons/version.Version=v1.0.0"
 //
 // For more info, see: http://stackoverflow.com/a/11355611/483528
 
 var (
-	VERSION = "latest"
+	Version = "latest"
 )
 
 // Version composes a version of the package
 func Version() string {
-	return VERSION
+	return Version
 }
