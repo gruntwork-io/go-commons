bf71dfaa345cb2501f8353ec1b875d73d16c9d19
Ina <ina.m.stoyanova@gmail.com>
2021-05-21 09:53:16 +0100

    Add unit-test skeleton

--- lock/lock_test.go
+++ lock/lock_test.go
@@ -9,23 +9,31 @@ import (
 "github.com/gruntwork-io/go-commons/logging"
 )
 
+
+
+//set up one run with a lock for resource X
+//set up another run with the same lock for resource X
+// -> is expected to wait
+// -> is expected to fail creating a new lock as the first one is still in use
+// the first lock should release
+
 func TestBlockingAcquireLock(t *testing.T) {
 	t.Parallel()
 
-	lockString := "guardduty1112-ina"
+	lockString := "guardduty-blocking-acquire-lock-test"
 
 	log := logging.GetLogger("")
+
 	defer ReleaseLock(log, lockString)
-	err := BlockingAcquireLock(lockString)
-	time.Sleep(2 * time.Minute)
+	//acquire the lock first time for resource X
+	err := BlockingAcquireLock(log, lockString)
+
+	time.Sleep(30 * time.Seconds)
 	if err != nil {
 		fmt.Println(err)
 	}
 
-	//set up one run with a lock for resource X
-	//set up another run with the same lock for resource X
-		// -> is expected to wait
-		// -> is expected to fail creating a new lock as the first one is still in use
-	// the first lock should release
+	//acquire the lock for resource X again shortly after
+	err := BlockingAcquireLock(log, lockString)
 
 }
