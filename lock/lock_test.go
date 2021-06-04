69c0caeb35e39a5dc77a05b205fa5c2887f55763
Ina <ina.m.stoyanova@gmail.com>
2021-06-04 17:15:14 +0100

    Updating test

--- lock/lock_test.go
+++ lock/lock_test.go
@@ -13,45 +13,43 @@ import (
 func TestAcquireLockWithRetries(t *testing.T) {
 	t.Parallel()
 
-	options *Options {
-
+	var options = Options {
+		logging.GetLogger("test"),
+		"test-dynamo-lock-eu-jam",
+		"guardduty-blocking-acquire-lock-test",
+		"eu-central-1",
+		2,
+		1 * time.Second,
+		true,
 	}
 
-	lockString := "guardduty-blocking-acquire-lock-test"
-	maxRetries := 2
-	lockTable := "test-dynamo-lock-eu-jam"
-	awsRegion := "eu-central-1"
-	sleepBetweenRetries := 1 * time.Second
+	defer assertLockReleased(t, &options)
+	defer ReleaseLock(&options)
 
-	log := logging.GetLogger("test")
-
-	defer assertLockReleased(t, lockString, lockTable, awsRegion)
-	defer ReleaseLock(log, lockString, lockTable, awsRegion)
-
-	log.Infof("Acquiring first lock")
-	err := AcquireLock(options)
+	options.Logger.Infof("Acquiring first lock")
+	err := AcquireLock(&options)
 	assert.NoError(t, err)
 
-	log.Infof("Acquiring second lock")
-	err = AcquireLock(options)
+	options.Logger.Infof("Acquiring second lock")
+	err = AcquireLock(&options)
 
 	if err == nil {
 		assert.Fail(t, "Acquiring of second lock succeeded, but it was expected to fail.")
 	}
 }
 
-func assertLockReleased(t *testing.T, lockString string, lockTable string, awsRegion string) {
-	dynamodbSvc, dbErr := NewDynamoDb(awsRegion)
+func assertLockReleased(t *testing.T, options *Options) {
+	client, dbErr := NewDynamoDb(options.AwsRegion)
 	assert.NoError(t, dbErr)
 
 	getItemParams := &dynamodb.GetItemInput{
 		Key: map[string]*dynamodb.AttributeValue{
-			"LockID": {S: aws.String(lockString)},
+			"LockID": {S: aws.String(options.LockString)},
 		},
-		TableName: aws.String(lockTable),
+		TableName: aws.String(options.LockTable),
 	}
 
-	item, err := dynamodbSvc.GetItem(getItemParams)
+	item, err := client.GetItem(getItemParams)
 	assert.NoError(t, err)
 
 	assert.Empty(t, item.Item)
