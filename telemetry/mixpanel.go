c1e986ce5b8bd5fb5fa3ae934806af6641362b05
Andrew Ellison <andrew@gruntwork.io>
2023-06-15 15:37:01 -0500

    Feature/telemetry forwarder (#90)
    * Update mixpanel telemetry to use forwarder
    
    * allow custom url
    
    * clean dependencies
    
    * Update telemetry/mixpanel.go
    
    Co-authored-by: Marina <marinaflessa@gmail.com>
    
    ---------
    
    Co-authored-by: Marina <marinaflessa@gmail.com>

--- telemetry/mixpanel.go
+++ telemetry/mixpanel.go
@@ -1,19 +1,20 @@
 package telemetry
 
 import (
+	"bytes"
+	"encoding/json"
 	"github.com/google/uuid"
 	"log"
+	"net/http"
 	"time"
-
-	"vizzlo.com/mixpanel"
 )
 
 type MixpanelTelemetryTracker struct {
-	clientId string
-	client   *mixpanel.Client
-	appName  string
-	version  string
-	runId    string
+	client  *http.Client
+	url     string
+	appName string
+	version string
+	runId   string
 }
 
 /*
@@ -42,20 +43,33 @@ func (m MixpanelTelemetryTracker) TrackEvent(eventContext EventContext, eventPro
 	// Combine our baseline props that we send for _ALL_ events with the passed in props from the event
 	trackProps := mergeMaps(baseProps, eventProps)
 
-	err := m.client.Track(m.runId, eventContext.EventName, trackProps)
-
+	request := map[string]interface{}{
+		"id":         m.runId,
+		"event":      eventContext.EventName,
+		"eventProps": trackProps,
+	}
+	jsonStr, err := json.Marshal(request)
+	if err != nil {
+		log.Println(err.Error())
+		return
+	}
+	resp, err := m.client.Post(m.url, "application/json", bytes.NewBuffer(jsonStr))
+	if err != nil {
+		log.Println(err.Error())
+		return
+	}
+	err = resp.Body.Close()
 	if err != nil {
 		log.Println(err.Error())
 	}
 }
 
-func NewMixPanelTelemetryClient(clientId string, appName string, version string) MixpanelTelemetryTracker {
-	mixpanelClient := mixpanel.New(clientId)
+func NewMixPanelTelemetryClient(url string, appName string, version string) MixpanelTelemetryTracker {
 	return MixpanelTelemetryTracker{
-		client:   mixpanelClient,
-		clientId: clientId,
-		appName:  appName,
-		version:  version,
-		runId:    uuid.New().String(),
+		client:  &http.Client{},
+		url:     url,
+		appName: appName,
+		version: version,
+		runId:   uuid.New().String(),
 	}
 }
